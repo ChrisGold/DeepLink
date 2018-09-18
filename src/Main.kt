d35e34a94d5a6c7d2f314b3bc7ced921c445d1fa
Christian Goldapp <c.goldapp@tu-bs.de>
2018-09-18 21:33:34 +0200

    Add UA recognition

--- src/Main.kt
+++ src/Main.kt
@@ -7,13 +7,26 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main(args: Array<String>) {
-    val server = embeddedServer(Netty, port = 8080) {
+    val server = embeddedServer(Netty, port = 8080, host = "192.168.0.10") {
         routing {
             get("/") {
                 val ua = call.request.userAgent() ?: "No user agent"
-                call.respondText(ua, ContentType.Text.Plain)
+                val type = determineDevice(ua)
+                call.respondText("$ua\n$type", ContentType.Text.Plain)
             }
         }
     }
     server.start(wait = true)
+}
+
+fun determineDevice(userAgent: String): ClientDevice =
+        if (arrayOf("iPhone", "iPad", "iPod").any { userAgent.contains(it) })
+            ClientDevice.IOS
+        else if (userAgent.contains("Android"))
+            ClientDevice.ANDROID
+        else
+            ClientDevice.OTHER
+
+enum class ClientDevice {
+    IOS, ANDROID, OTHER
 }
